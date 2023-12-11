80e82e1b9800fbfe8aa32f83883f3e417e6705c0
lvkunahang <kylelv@pku.edu.cn>
2023-12-11 15:14:26 +0800

    psmt

---
+++ print.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+THREAD_NUM=20
+
+#指定分隔符
+IFS='
+'
+#定义描述符为9的FIFO管道
+mkfifo tmp
+exec 9<>tmp
+rm -f tmp
+ 
+#预先写入指定数量的空格符，一个空格符代表一个进程
+for ((i=0;i<$THREAD_NUM;i++))
+do
+    echo >&9
+done
+
+function parallel() {
+    for folder in $@/*;do 
+        if [ ! -d results/$folder ];then
+            mkdir results/$folder
+        fi
+        state=-1
+        for file in $folder/*;do
+            if test -f $file;then
+                state=0
+                break
+            fi
+            if test -d $file;then
+                state=1
+                break
+            fi
+        done
+        if [ $state -eq 0 ];then
+            for i in $folder/*; do
+                echo $i
+                # start=$[$(date +%s%N)/1000000]
+                # timeout 1800 
+                # python3 src/print.py $i
+                # end=$[$(date +%s%N)/1000000]
+                # take=$(( end - start ))
+                # echo $i , ${take} ms >> times.csv
+                read -u9
+                {
+                    python3 src/print.py $i
+                    sleep 1
+                    #控制进程数：一个任务完成后，写入一个空格字符到管道，新的任务将可以执行
+                    echo >&9
+                }&
+            done
+        fi
+        if [ $state -eq 1 ];then
+            parallel $folder
+        fi
+    done
+}
+
+parallel $1
+
+wait
+echo "\n全部任务执行结束"
---
+++ src/print.py
@@ -0,0 +1,205 @@
+from pysmt.smtlib.parser import SmtLibParser, get_formula
+import pysmt.smtlib.commands as smtcmd
+import argparse
+import time
+import torch
+from smt2tensor import myTensor
+from tqdm import tqdm
+import re
+import os
+import numpy as np
+from sklearn.cluster import KMeans
+import numpy as np
+
+DEBUG = False
+MAXWORKERS = None       # ProcessPoolExecutor default is None
+DIM = 1024
+Z3TIMELIMIT = 10000     # ms
+THREADTIMELIMIT = Z3TIMELIMIT/1000
+PROCESSTIMELIMIT = THREADTIMELIMIT + 1
+ITERTIMELIMIT = 600     # s
+Epochs = 600
+Lr = 0.5
+USEEPS = False
+EPS = 0.0001
+
+
+def parse_args():
+    arg_parser = argparse.ArgumentParser(description='SMT_GD')
+    arg_parser.add_argument('path', type=str, help='path of smt-lib file')
+    arg_parser.add_argument("-D", "--debug", action="store_true", help="enable debug mode")
+    arg_parser.add_argument("-W", "--workers", type=int, default=None, help="number of parallel workers")
+    args_get = arg_parser.parse_args()
+    return args_get
+
+
+mypath=""
+
+def get_smt_script(path):
+    smt_parser = SmtLibParser()
+    global mypath
+    mypath = path
+    with open(path, 'r') as fp:
+        script_get = smt_parser.get_script(fp)
+        smt_logic = smt_parser.logic
+    return script_get, smt_logic
+
+
+def get_smt_formula(path):
+    with open(path, 'r') as fp:
+        f = get_formula(fp)
+    return f
+
+parameter_val={}
+def generate_parameter(declaration):
+    val = parameter_val[declaration.strip()]
+    return f"{val[0]} {val[1]}"
+
+def MyPrint(init_result, outtime):
+    global parameter_val
+    for key_, (vals_, grads_) in init_result.items():
+        data = vals_
+        mean = np.mean(data)
+        std = np.std(data)
+        val = (float(format(mean, '.2g')), float(format(std, '.2g')))
+        parameter_val[key_]=val
+
+    with open(mypath, 'r') as file:
+        smt2_content = file.read()
+    smt2_content = re.sub(r'(declare-fun (.+?)\(\) Real)', lambda m: f'{m.group(0)} GD {generate_parameter(m.group(2))}', smt2_content)
+    smt2_content = re.sub(r'(declare-const (.+?) Real)', lambda m: f'{m.group(0)} GD {generate_parameter(m.group(2))}', smt2_content)
+    # print(smt2_content)
+
+    file_path = str(outtime)+"/"+ mypath
+    folder_path = os.path.dirname(file_path)
+    # print(folder_path)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    with open(file_path, 'w') as file:
+        file.write(smt2_content)
+
+
+
+
+def adjust_learning_rate(optimizer, epoch, lr):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+
+    if (epoch % 50 == 0) and (epoch > 0):
+        lr = lr * (0.5 ** (epoch // 50))
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+
+def init_tensor(script):
+    mytensor = myTensor()
+    for cmd in script:
+        if cmd.name in [smtcmd.DECLARE_FUN, smtcmd.DECLARE_CONST]:
+            symbol = cmd.args[0]
+            mytensor.parse_declare(symbol)
+        elif cmd.name == smtcmd.ASSERT:
+            node = cmd.args[0]
+            mytensor.parse_assert(node)
+    mytensor.init_tensor(DIM)
+    return mytensor
+
+
+def clustering(data, num_clusters):
+    data_array = np.array([[vals_[i] for key_, (vals_, grads_) in data.items()] for i in range(DIM)])
+    # print(data_array)
+    kmeans = KMeans(n_clusters=num_clusters)
+    kmeans.fit(data_array)
+    labels = kmeans.labels_
+    cluster_stats = []
+    for i in range(num_clusters):
+        cluster_data = data_array[labels == i]  # 获取属于当前簇的数据
+        cluster_min = np.min(cluster_data, axis=0)  # 取值下界
+        cluster_max = np.max(cluster_data, axis=0)  # 取值上界
+        cluster_count = len(cluster_data)  # 数据数量
+        cluster_stats.append((cluster_min, cluster_max, cluster_count))
+    
+    cluster_stats = sorted(cluster_stats, key=lambda x: -x[2])
+    return cluster_stats
+
+
+
+def generate_init_solution(mytensor):
+    global Epochs, Lr
+    init_result = {}
+    mytensor.init_val(DIM)
+    T1 = time.process_time()
+    y = mytensor.pre_sol()  # 化简常量，顺便记录sol时间
+    T2 = time.process_time()
+    if T2-T1 > 0.8:
+        new_epochs = int(ITERTIMELIMIT*0.5/(T2-T1))
+        Lr *= new_epochs/Epochs
+        Epochs = new_epochs
+
+    if DEBUG:
+        print('程序运行时间1:%s毫秒' % ((T2 - T1)*1000))
+        print(Epochs)
+
+    optimizer = torch.optim.Adam([mytensor.vars], lr=Lr)
+
+    _out_cnt = 100###
+    for step in tqdm(range(Epochs)) if DEBUG else range(Epochs):
+        adjust_learning_rate(optimizer, step, Lr)
+        optimizer.zero_grad()
+        y = mytensor.sol()
+
+        y.backward(torch.ones(DIM))
+        for name in mytensor.names:
+            init_result[name] = (mytensor.vars[mytensor.namemap[name][0]].tolist(),
+                                 mytensor.vars.grad[mytensor.namemap[name][0]])
+            
+
+        T2 = time.process_time()
+        if T2-T1 > 100 and _out_cnt == 100:
+            # MyPrint(init_result,_out_cnt)
+            _out_cnt=300
+        elif T2-T1 > 300 and _out_cnt == 300:
+            # MyPrint(init_result,_out_cnt)
+            _out_cnt=600
+
+
+        if torch.any(y < torch.zeros(DIM)):
+            return None
+        if T2-T1 > ITERTIMELIMIT:
+            break
+        optimizer.step()
+
+    # for i in range(DIM):
+    #     for key_, (vals_, grads_) in init_result.items():
+    #         print(key_,":",vals_[i])
+    # print("")
+    # MyPrint(init_result,_out_cnt)
+    
+    clustering(init_result, 30)
+
+    return init_result
+
+
+
+
+def solve(path):
+    script, smt_logic = get_smt_script(path)
+    formula = get_smt_formula(path)
+
+    mytensor = init_tensor(script)
+    init_result = generate_init_solution(mytensor)
+
+    # if init_result is None:         # sat
+    #     print("sat")
+    # else:
+    #     res = parallel_sol(init_result, mytensor, formula, smt_logic)
+    #     print("sat" if res else "NONE")
+
+
+if __name__ == "__main__":
+    T1 = time.perf_counter()
+    args = parse_args()
+    DEBUG = args.debug
+    MAXWORKERS = args.workers
+    
+    solve(args.path)
+    if DEBUG:
+        print(time.perf_counter()-T1)
